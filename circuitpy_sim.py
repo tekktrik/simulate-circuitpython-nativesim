db129c15fa2e51493f7029e7670313001f61a434
Alec Delaney <tekktrik@gmail.com>
2026-03-15 20:25:02 -0400

    Convert simulator methods to functions

--- circuitpy_sim.py
+++ circuitpy_sim.py
@@ -10,115 +10,112 @@ import subprocess
 import sys
 
 
-class Simualtor:
-    """Zephyr OS native sim wrapper."""
-
-    @staticmethod
-    def simulate(firmware_filepath: str, flash_filepath: str, timeout: int = 5) -> str:
-        """Simulate using the native sim firmware."""
-        firmware_path = pathlib.Path(firmware_filepath).absolute()
-        flash_path = pathlib.Path(flash_filepath).absolute()
-        
-        cmd = [
-            str(firmware_path),
-            f"--flash={str(flash_path)}",
-            "-rt",
-            "-uart_stdinout",
-            f"-stop_at={timeout}",
-        ]
-
-        simproc = subprocess.Popen(
-            cmd,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=None,
-        )
-
-        if simproc.stdout is None:
-            raise RuntimeError("Failed to capture simulator output")
-
-        recording: bool | None = None
-        recorded = ""
-
-        while simproc.poll() is None:
-            pass
-
-        output: str = simproc.stdout.read().decode()
-        for line in output.split("\n"):
-            encoded = line.encode()
-            if not line:
-                continue
-            if (
-                encoded.strip() == b"\x1b[2K\x1b[0Gcode.py output:"
-                and recording is None
-            ):
-                recording = True
-            elif line.strip() == "Code done running." and recording:
-                recording = False
-                break
-            elif recording:
-                recorded += line + "\n"
-
-        return recorded.strip()
+def simulate(firmware_filepath: str, flash_filepath: str, timeout: int = 5) -> str:
+    """Simulate using the native sim firmware."""
+    firmware_path = pathlib.Path(firmware_filepath).absolute()
+    flash_path = pathlib.Path(flash_filepath).absolute()
     
-    @staticmethod
-    def prepare_flash(flash_filepath: str, circuitpy_filepath: str) -> None:
-        """Prepare the native sim flash."""
-        flash_path = str(pathlib.Path(flash_filepath).absolute())
-        circuitpy_abspath = pathlib.Path(circuitpy_filepath).absolute()
+    cmd = [
+        str(firmware_path),
+        f"--flash={str(flash_path)}",
+        "-rt",
+        "-uart_stdinout",
+        f"-stop_at={timeout}",
+    ]
 
-        subprocess.run(
-            [
-                "truncate",
-                "-s",
-                "2M",
-                f"{ flash_path }",
-            ],
-            timeout=5,
-            check=True,
-        )
+    simproc = subprocess.Popen(
+        cmd,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=None,
+    )
 
-        subprocess.run(
-            [
-                "mformat",
-                "-i",
-                f"{ flash_path }",
-                "::",
-            ],
-            timeout=5,
-            check=True,
-        )
+    if simproc.stdout is None:
+        raise RuntimeError("Failed to capture simulator output")
 
-        mcopy_cmd = [
-            "mcopy",
+    recording: bool | None = None
+    recorded = ""
+
+    while simproc.poll() is None:
+        pass
+
+    output: str = simproc.stdout.read().decode()
+    for line in output.split("\n"):
+        encoded = line.encode()
+        if not line:
+            continue
+        if (
+            encoded.strip() == b"\x1b[2K\x1b[0Gcode.py output:"
+            and recording is None
+        ):
+            recording = True
+        elif line.strip() == "Code done running." and recording:
+            recording = False
+            break
+        elif recording:
+            recorded += line + "\n"
+
+    return recorded.strip()
+
+def prepare_flash(flash_filepath: str, circuitpy_filepath: str) -> None:
+    """Prepare the native sim flash."""
+    flash_path = str(pathlib.Path(flash_filepath).absolute())
+    circuitpy_abspath = pathlib.Path(circuitpy_filepath).absolute()
+
+    subprocess.run(
+        [
+            "truncate",
             "-s",
+            "2M",
+            f"{ flash_path }",
+        ],
+        timeout=5,
+        check=True,
+    )
+
+    subprocess.run(
+        [
+            "mformat",
             "-i",
             f"{ flash_path }",
-        ]
+            "::",
+        ],
+        timeout=5,
+        check=True,
+    )
 
-        files = [str(path) for path in circuitpy_abspath.glob("*")]
-        mcopy_cmd.extend(files)
-        mcopy_cmd.append("::")
-        
-        subprocess.run(
-            mcopy_cmd,
-            timeout=5,
-            check=True,
-        )
+    mcopy_cmd = [
+        "mcopy",
+        "-s",
+        "-i",
+        f"{ flash_path }",
+    ]
+
+    files = [str(path) for path in circuitpy_abspath.glob("*")]
+    mcopy_cmd.extend(files)
+    mcopy_cmd.append("::")
+    
+    subprocess.run(
+        mcopy_cmd,
+        timeout=5,
+        check=True,
+    )
 
 
-def simulate_circuitpython() -> None:
+def simulate_circuitpython_cmdline() -> None:
     """Simulate CircuitPython using a simulator."""
-    result = Simualtor.simulate(sys.argv[1], sys.argv[2])
+    firmware_filepath = sys.argv[1]
+    flash_filepath = sys.argv[2]
+    result = simulate(firmware_filepath, flash_filepath)
     print(result)
 
 
-def prepare_flash() -> None:
+def prepare_flash_cmdline() -> None:
     """PRepare flash for the simulator."""
     flash_filepath = sys.argv[1]
     circuitpy_filepath = sys.argv[2]
-    Simualtor.prepare_flash(flash_filepath, circuitpy_filepath)
+    prepare_flash(flash_filepath, circuitpy_filepath)
 
 
 if __name__ == "__main__":
-    simulate_circuitpython()
+    simulate_circuitpython_cmdline()
